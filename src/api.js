1701bbae1d6e4617fb5c0983813d5bc78db8840a
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-03-22 14:07:00 -0700

    fix nested API calls and make search work smoothly

--- src/api.js
+++ src/api.js
@@ -17,10 +17,10 @@ export class LocationSearch{
 }
 
 export class DoctorSearch{
-  getDoctor(type, search) {
+  getDoctor(location, type, search) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5122,-122.6587,50&${type}=${search}&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&${type}=${search}&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -11,44 +11,41 @@ $(function(){
 
   $(".search").submit(function(event){
     event.preventDefault();
-
     $('.results').empty();
 
-    let locationRaw = $('.location').val();
-    const location = concatLocation(locationRaw);
+    const distance = $('.distance').val();
+    const location = concatLocation($('.location').val());
+    const searchType = $('.selection').val();
+    const searchParameters = $(".searchTerm").val();
 
     const getGeocode = locationApi.getLocation(location);
-
-
     getGeocode.then(function(response) {
       let body = JSON.parse(response);
-      console.log(body);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+      const geocode = `${body.results[0].geometry.location.lat},${body.results[0].geometry.location.lng},${distance}`;
 
-    const searchType = $('.selection').val();
-    const searchParameters = $(".searhTerm").val();
+      const getDoctors = doctorApi.getDoctor(geocode, searchType, searchParameters);
 
-    const getDoctors = doctorApi.getDoctor(searchType, searchParameters);
-
-    getDoctors.then(function(response) {
-      let body = JSON.parse(response);
-      console.log(body);
-      for(let i=0; i<body.meta.count; i++){
-        $('.results').append(`<div class="result">
+      getDoctors.then(function(response) {
+        let body = JSON.parse(response);
+        console.log(body);
+        for(let i=0; i<body.meta.count; i++){
+          $('.results').append(`<div class="result">
           <img src="${body.data[i].profile.image_url}" alt="picture of doctor">
           <div>
-            <p>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}</p>
-            <p>${body.data[i].practices[0].visit_address.street}<br>${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}</p>
-            <p>${body.data[i].practices[0].phones[0].number}</p>
-            <a href="${body.data[i].practices[0].website}">Visit Website</a>
-            <p>Currently Accepting Patients: ${body.data[i].practices[0].accepts_new_patients}</p>
+          <p>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}</p>
+          <p>${body.data[i].practices[0].visit_address.street}<br>${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}</p>
+          <p>${body.data[i].practices[0].phones[0].number}</p>
+          <a href="${body.data[i].practices[0].website}">Visit Website</a>
+          <p>Currently Accepting Patients: ${body.data[i].practices[0].accepts_new_patients}</p>
           </div>
           </div>`)
-      }
+        }
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
+
   });
 });
