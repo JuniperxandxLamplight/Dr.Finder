c8d4c0e9ea71f13ba517a255f23973efc35573a3
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-03-22 09:13:54 -0700

    add api call basic logic, currently error 401 (unauthorized)

---
+++ src/api.js
@@ -0,0 +1,17 @@
+export class DoctorSearch{  
+  getDoctor(search) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_location=45.5122,122.6587&query=${search}&user_key=process.env.exports.apiKey`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {DoctorSearch} from './api.js';
+
+$(function(){
+  const apiCall = new DoctorSearch;
+
+  $(".search").submit(function(event){
+    event.preventDefault();
+
+    let searchParameters = $(".input1").val();
+
+    const getDoctors = apiCall.getDoctor(searchParameters);
+
+    getDoctors.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
