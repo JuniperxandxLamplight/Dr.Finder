6dc8e1a87b29922c5c1f15fa255f32db0bbd5ca2
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-03-22 09:39:14 -0700

    get api call working with input query

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-export class DoctorSearch{  
+export class DoctorSearch{
   getDoctor(search) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_location=45.5122,122.6587&query=${search}&user_key=process.env.exports.apiKey`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5122,-122.6587,50&query=${search}&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
